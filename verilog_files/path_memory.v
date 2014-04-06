a747a7074a78ca7a68ff3628676779f824308ab5
SreenivasPulikonda <psreenivas98@gmail.com>
2014-04-06 23:08:03 +0530

    path memory and reduce

---
+++ verilog_files/path_memory.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:09:07 03/27/2014 
+// Design Name: 
+// Module Name:    path_memory 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module path_memory(p0, p1, p2, p3, path0, clk, reset, ACS0, ACS1, ACS2, ACS3);
+    output [2:0] p0;
+    output [2:0] p1;
+    output [2:0] p2;
+    output [2:0] p3;
+    input [11:0] path0;
+    input clk;
+    input reset;
+    input ACS0;
+    input ACS1;
+    input ACS2;
+    input ACS3;
+	 wire [11:0]out1,out2,out3,out4,out5,out6,out7,out8,out9,out10,out11;
+	path x1 (path0,out1 ,clk,reset,ACS0,ACS1,ACS2,ACS3),
+	x2 (out1, out2 ,clk,reset,ACS0,ACS1,ACS2,ACS3),
+	x3 (out2, out3 ,clk,reset,ACS0,ACS1,ACS2,ACS3),
+	x4 (out3, out4 ,clk,reset,ACS0,ACS1,ACS2,ACS3),
+	x5 (out4, out5 ,clk,reset,ACS0,ACS1,ACS2,ACS3),
+	x6 (out5, out6 ,clk,reset,ACS0,ACS1,ACS2,ACS3),
+	x7 (out6, out7 ,clk,reset,ACS0,ACS1,ACS2,ACS3),
+	x8 (out7, out8 ,clk,reset,ACS0,ACS1,ACS2,ACS3),
+	x9 (out8, out9 ,clk,reset,ACS0,ACS1,ACS2,ACS3),
+	x10(out9, out10,clk,reset,ACS0,ACS1,ACS2,ACS3),
+	x11(out10,out11,clk,reset,ACS0,ACS1,ACS2,ACS3);
+	assign p0 = out11[11:9];
+	assign p1 = out11[ 8:6];
+	assign p2 = out11[ 5:3];
+	assign p3 = out11[ 2:0];
+	
+	
+endmodule
---
+++ verilog_files/reduce.v
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:36:47 03/27/2014 
+// Design Name: 
+// Module Name:    reduce 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module reduce(in0, in1, in2, in3, m_in0, m_in1, m_in2, m_in3, control);
+    input [4:0] in0;
+    input [4:0] in1;
+    input [4:0] in2;
+    input [4:0] in3;
+    output [4:0] m_in0;
+    output [4:0] m_in1;
+    output [4:0] m_in2;
+    output [4:0] m_in3;
+    output [1:0] control;
+	wire [4:0] smallest;
+function [4:0] find_smallest;
+input [4:0] in0,in1,in2,in3; reg [4:0] a,b;
+begin
+if(in0 <= in1) a = in0; else a = in1;
+if(in2 <= in3) b = in2; else b = in3;
+if(a <= b) find_smallest = a;
+else find_smallest = b;
+end
+endfunction
+function [1:0] smallest_no;
+input [4:0] in0,in1,in2,in3,smallest;
+begin
+if(smallest == in0) smallest_no = 0;
+else if (smallest == in1) smallest_no = 1;
+else if (smallest == in2) smallest_no = 2;
+else smallest_no = 3;
+end
+endfunction
+assign smallest = find_smallest(in0,in1,in2,in3);
+assign m_in0 = in0 - smallest;
+assign m_in1 = in1 - smallest;
+assign m_in2 = in2 - smallest;
+assign m_in3 = in3 - smallest;
+assign control = smallest_no(in0,in1,in2,in3,smallest);
+
+endmodule
