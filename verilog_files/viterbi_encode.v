c8445550c974f2e0956f5d034dbdc49dbc0319c5
Sreenivas Pulikonda <psreenivas98@gmail.com>
2014-04-10 21:31:16 +0530

    Update viterbi_encode.v
    modified

--- verilog_files/viterbi_encode.v
+++ verilog_files/viterbi_encode.v
@@ -18,7 +18,8 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module viterbi_encode(X2N, X1N, Y2N, Y1N, Y0N, clk, res);
+module viterbi_encode(X2N, X1N, Y2N, Y1N, Y0N, clk, res);  	//(2/3) encoder. X2N and X1N are the inputs
+								//Y2N, Y1N, Y0N are the outputs of the encoder
     input X2N;
     input X1N;
     output Y2N;
@@ -26,12 +27,13 @@ module viterbi_encode(X2N, X1N, Y2N, Y1N, Y0N, clk, res);
     output Y0N;
     input clk;
     input res;
-	 wire X1N_1, X2N_2, Y2N, Y1N, Y0N;
-	 dff dff_1(X1N,X1N_1,clk,res); 
-	 dff dff_2(X1N_1,X1N_2,clk,res);
-	 assign Y2N=X2N; 
-	 assign Y1N=X1N ^ X1N_2; 
-	 assign Y0N=X1N_1;
+    wire X1N_1, X2N_2, Y2N, Y1N, Y0N;
 
+    dff dff_1(X1N,X1N_1,clk,res); 				//Using D Flipflops as the Shift registers
+    dff dff_2(X1N_1,X1N_2,clk,res);
+    
+    assign Y2N = X2N; 
+    assign Y1N = X1N ^ X1N_2; 
+    assign Y0N = X1N_1;
 
 endmodule
